66fbd39af3cc7f8ddcaf89d1c9f2bcf3bb15ac13
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2023-02-14 22:00:54 +0000

    Feature(ServerApp) Se creo un while de prueba

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -1,5 +1,18 @@
 package server;
 
+//import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.ArrayList;
+
 public class ServerApp {
-    
+    public static void main(String[] args) {
+        
+            while (true) {
+                System.out.println("========================");
+                System.out.println("Esperando por cliente...");
+            }
+    }
 }
