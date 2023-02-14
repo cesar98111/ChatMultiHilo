874bbbaeb722818f58c9d0ff037faa29d18c75d4
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2023-02-14 22:05:02 +0000

    Feature(ServerApp) Se creo try catch y nuevo ServerSocket

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -4,15 +4,29 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import shared.Constants;
 
 import java.util.ArrayList;
 
 public class ServerApp {
     public static void main(String[] args) {
-        
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(Constants.SERVER_PORT);
+
             while (true) {
                 System.out.println("========================");
                 System.out.println("Esperando por cliente...");
+
             }
+
+            // System.out.println("========================");
+            // System.out.println("Cerrando el servidor");
+            // serverSocket.close();
+        
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
