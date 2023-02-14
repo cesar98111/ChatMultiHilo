7b6d433c09c46be44ed2e6e5c1fa000f2faea164
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2023-02-14 22:17:23 +0000

    Feature(ClientHandler) Se implementa el metodo run

--- src/server/threads/ClientHandler.java
+++ src/server/threads/ClientHandler.java
@@ -12,6 +12,27 @@ public class ClientHandler extends Thread {
 
     @Override
     public void run() {
+        try {
+            System.out.println("========================");
+            String msg = "La conexión se ha realizado con ";
+            msg += this.clientSocket.getInetAddress().toString();
+            System.out.println(msg);
 
+            System.out.println("========================");
+            DataInputStream fromClientStream = new DataInputStream(this.clientSocket.getInputStream());
+            String clientName = fromClientStream.readUTF();
+
+            while (true) {
+                String clientMessage = fromClientStream.readUTF();
+                System.out.println(clientName + " dice: " + clientMessage);
+                if (clientMessage.equals("bye")) {
+                    break;
+                }
+            }
+            fromClientStream.close();
+            System.out.println("-->" + clientName + " se ha ido.");
+        } catch (IOException e) {
+            System.out.println("Oh no, esesión!");
+        }
     }
 }
